be09c1ae6f245bc2df62de03d832288b7e9916b3
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-11 18:15:38 +0200

    yeah working fine

--- src/course/interfaces/course.interface.ts
+++ src/course/interfaces/course.interface.ts
@@ -1,7 +1,6 @@
 import { Document } from 'mongoose';
-import { User } from 'src/user/interfaces/user.interface';
 
 export interface Course extends Document {
   readonly name: string;
-  readonly author: [User];
+  readonly author: [string];
 }
