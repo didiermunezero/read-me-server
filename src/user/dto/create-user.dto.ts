75dde793ae42c03d20efcaeb2f56c424e1b4d921
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-11 08:16:55 +0200

    adding user dto object to the defininiton

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { ObjectType, Field, Int, ID } from 'type-graphql';
+
+@ObjectType()
+export class UserType {
+  @Field(() => ID)
+  id: string;
+  @Field()
+  readonly username: string;
+  @Field()
+  readonly fname: string;
+  @Field()
+  readonly lname: string;
+  @Field()
+  readonly dob: string;
+  @Field()
+  readonly profile: string;
+}
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,31 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Subscription,Query, Mutation, Args,} from '@nestjs/graphql';
+import {PubSub} from 'apollo-server-express'
+import { UserService } from './user.service';
+import { UserType } from './dto/create-user.dto';
+import { CreateInput } from './inputs/create.input';
+const pubSub = new PubSub();
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private readonly catsService: UserService) {}
+
+  @Query(() => String)
+  async hello() {
+    return 'hello';
+  }
+
+  @Query(() => [UserType])
+  async cats() {
+    return this.catsService.findAll();
+  }
+
+  @Mutation(() => UserType)
+  async createCat(@Args('input') input: CreateInput) {
+    pubSub.publish('newCat', { newCat: input });
+    return this.catsService.create(input);
+  }
+  @Subscription(() => UserType)
+  newCat() {
+    return pubSub.asyncIterator('newCat');
+  }
+}
