6c4758f5edc3b9afa26a5270dc3b1d95cb8e53b3
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-11 07:54:24 +0200

    user interface added

---
+++ src/user/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+import { Document } from 'mongoose';
+
+export interface User extends Document {
+  readonly username: string;
+  readonly fname: string;
+  readonly lname: string;
+  readonly dob: string;
+  readonly profile: string;
+  readonly type: string;
+}
