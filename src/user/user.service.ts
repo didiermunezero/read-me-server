4b70e2eaf3473598456f30ca8eebcef298243db2
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-11 07:58:56 +0200

    user service definition

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './interfaces/user.interface';
+import { CreateInput } from './inputs/create.input';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async create(createDto: CreateInput): Promise<User> {
+    const createdCat = new this.userModel(createDto);
+    return await createdCat.save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.userModel.find().exec();
+  }
+}
