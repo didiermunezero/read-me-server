12c8acb265df4f20899f19519abe64879b7e02a0
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-13 11:36:38 +0200

    joi validator added

--- src/user/inputs/joi/create.joi.ts
+++ src/user/inputs/joi/create.joi.ts
@@ -1,11 +1,11 @@
 import * as Joi from 'joi'
 export function userValidator(user){
-const schema = Joi.schema({
+const schema = Joi.object({
     username: Joi.string().min(5).max(30).required(),
     lname: Joi.string().min(5).max(30).required(),
     fname: Joi.string().min(5).max(30).required(),
     password: Joi.string().min(3).max(30).required(),
-    email: Joi.email().min(8).max(30).required(),
+    email: Joi.string().min(8).max(30).required(),
     profile: Joi.string(),
     dob: Joi.date(),
 })
@@ -13,7 +13,7 @@ return schema.validate(user);
 }
 
 export function loginValidator(user){
-    const schema = Joi.schema({
+    const schema = Joi.object({
         username: Joi.string().min(5).max(30).required(),
         password: Joi.string().min(3).max(30).required(),
     })
