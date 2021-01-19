7fc9b72610b271c1e010c5475444143119fa7d9d
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-19 13:37:44 +0200

    changes for heroku

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(2500);
+  await app.listen(process.env.PORT || 2500);
 }
 bootstrap();
