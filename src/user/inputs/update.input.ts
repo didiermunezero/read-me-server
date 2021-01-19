a5db7a84c60b75e96351aa4334537ec18b5ec43a
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-19 07:22:26 +0200

    no id is needed on update user

--- src/user/inputs/update.input.ts
+++ src/user/inputs/update.input.ts
@@ -2,26 +2,6 @@ import { InputType, Field,ID} from 'type-graphql';
 
 @InputType()
 export class updateInput {
-    @Field(() => ID)
-   _id: string
-  @Field({ nullable: true })
-   email: string;
-  @Field({ nullable: true })
-  readonly username: string;
-  @Field({ nullable: true })
-  readonly fname: string;
-  @Field({ nullable: true })
-  readonly lname: string;
-  @Field({ nullable: true })
-  readonly dob: string;
-  @Field({ nullable: true })
-  readonly profile: string;
-  @Field()
-   password: string;
-}
-
-@InputType()
-export class updateInputNoId {
   @Field({ nullable: true })
    email: string;
   @Field({ nullable: true })
